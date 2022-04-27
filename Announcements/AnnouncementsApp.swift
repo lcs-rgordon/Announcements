d750bcdc4fa3c85b9cab855ae4b2494843d46096
Russell Gordon <github@russellgordon.ca>
2022-04-27 17:20:37 -0400

    Organized project files into groups.

--- Announcements/AnnouncementsApp.swift
+++ Announcements/AnnouncementsApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct AnnouncementsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                AnnouncementDetailView()
+            }
         }
     }
 }
---
+++ Announcements/Views/AnnouncementDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  AnnouncementDetailView.swift
+//  Announcements
+//
+//  Created by Russell Gordon on 2022-04-27.
+//
+
+import SwiftUI
+
+struct AnnouncementDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnnouncementDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AnnouncementDetailView()
+        }
+    }
+}
