30fc4fec6035debaa614f0760e84909c7a9bd5fe
Russell Gordon <github@russellgordon.ca>
2022-04-27 17:18:18 -0400

    Initial Commit

---
+++ Announcements/AnnouncementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnouncementsApp.swift
+//  Announcements
+//
+//  Created by Russell Gordon on 2022-04-27.
+//
+
+import SwiftUI
+
+@main
+struct AnnouncementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
