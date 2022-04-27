24f6f6a505a17c044f4625a80c894e81be426c1a
Russell Gordon <github@russellgordon.ca>
2022-04-27 17:22:42 -0400

    Created static layout for a single announcement.
    Based on data at:
    
    https://www.russellgordon.ca/lcs/2021-22/sheety-demo/announcement.json

--- Announcements/Views/AnnouncementDetailView.swift
+++ Announcements/Views/AnnouncementDetailView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct AnnouncementDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 30) {
+
+                VStack(alignment: .leading, spacing: 5) {
+                    HStack {
+                        Text("**Posted by:** David Crispo")
+                        Spacer()
+                    }
+                    .font(.caption)
+                    
+                    Text("4/27/2022")
+                        .font(.caption)
+                }
+                
+                RemoteImageView(fromURL: URL(string: "https://www.russellgordon.ca/lcs/2021-22/sheety-demo/airband.jpg")!)
+
+                Text("Good morning, all houses should now have a time for an Airbands sound check in the theatre. \n\nSome important reminders:\n\n• The time slots are only 15 minutes, please make sure that you're there on time and you leave your bags/coats in the theatre lobby so we can get everyone through. \n\n• There are pieces of the set still in the theatre, please do not touch any of it.  \n\nIn the words of Martha Graham, \"Dance is the hidden language of the soul\" \n\nThanks everyone!")
+            }
+            .padding(.horizontal)
+        }
+        .navigationTitle("Airbands update 🎸")
     }
 }
 
